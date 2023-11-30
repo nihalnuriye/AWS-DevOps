ae174fc3eeceb7816efa25569c31132694d46833
nihalnuriye <nihalnuriyeerturk@gmail.com>
2023-11-30 20:20:50 +0100

    orjinale döndürüldü

--- AWS/projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ AWS/projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,24 +1,9 @@
 from flask import Flask 
 app = Flask(__name__)
 
-@app.route('/')
-def head():
-     return 'Hello World Nihal'
 
 
-@app.route('/second')
-def second():
-     return 'This is second page'
 
-
-@app.route('/third')
-def third():
-     return 'This is third page'
-
-
-@app.route('/fourth/<string:id>')
-def fourth(id):
-     return f'Id of this page is {id}'
 
 
 if __name__ == '__main__':
